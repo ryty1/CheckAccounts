25d4be56fdc152e1d72874ffe71590b13d74efb5
人皇帝辛 <120008435@qq.com>
2025-01-23 01:04:50 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -13,14 +13,26 @@ PUBLIC_NODEJS_DIR="$DOMAIN_DIR/public_nodejs"
 APP_JS_PATH="$PUBLIC_NODEJS_DIR/app.js"
 APP_JS_URL="https://raw.githubusercontent.com/ryty1/htmlalive/main/app.js"
 
-echo "正在删除默认域名"
+echo "正在删除默认域名..."
 devil www del "$DOMAIN" 2>/dev/null
+if [[ $? -eq 0 ]]; then
+    echo "默认域名已成功删除。"
+else
+    echo "默认域名删除失败，可能不存在。"
+fi
+
 if [[ -d "$DOMAIN_DIR" ]]; then
     rm -rf "$DOMAIN_DIR"
 fi
 
-echo "正在生成新域名 $DOMAIN..."
-devil www add "$DOMAIN" nodejs /usr/local/bin/node22
+echo "正在申请域名……"
+if devil www add "$DOMAIN" nodejs /usr/local/bin/node22; then
+    echo " $DOMAIN 已生成。"
+else
+    echo "新域名生成失败，请检查环境配置。"
+    exit 1
+fi
+
 if [[ ! -d "$PUBLIC_NODEJS_DIR" ]]; then
     mkdir -p "$PUBLIC_NODEJS_DIR"
 fi
@@ -43,7 +55,6 @@ fi
 
 chmod 644 "$APP_JS_PATH"
 if [[ $? -eq 0 ]]; then
-    echo "文件权限设置成功"
 else
     echo "文件权限设置失败"
     exit 1
@@ -52,4 +63,3 @@ fi
 echo "一键部署已完成。"
 echo " "
 echo "保活地址 https://$DOMAIN/info"
-
