f1c2e3c3424309880d883b168600d485511e4e88
人皇帝辛 <120008435@qq.com>
2025-01-23 00:57:27 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if [[ -z "$USERNAME" ]]; then
     echo "无法获取当前系统用户名，脚本退出。"
     exit 1
 fi
+
 DOMAIN="$USERNAME.serv00.net"
 NODE_PORT=3000
 DOMAIN_FOLDER_ROOT="/home/$USERNAME/domains"
@@ -11,16 +12,19 @@ DOMAIN_DIR="$DOMAIN_FOLDER_ROOT/$DOMAIN"
 PUBLIC_NODEJS_DIR="$DOMAIN_DIR/public_nodejs"
 APP_JS_PATH="$PUBLIC_NODEJS_DIR/app.js"
 APP_JS_URL="https://raw.githubusercontent.com/ryty1/htmlalive/main/app.js"
+
 echo "正在删除默认域名"
 devil www del "$DOMAIN" 2>/dev/null
 if [[ -d "$DOMAIN_DIR" ]]; then
     rm -rf "$DOMAIN_DIR"
 fi
+
 echo "正在生成新域名 $DOMAIN..."
 devil www add "$DOMAIN" nodejs /usr/local/bin/node22
 if [[ ! -d "$PUBLIC_NODEJS_DIR" ]]; then
     mkdir -p "$PUBLIC_NODEJS_DIR"
 fi
+
 echo "正在安装依赖………"
 if npm install dotenv basic-auth express > /dev/null; then
     echo "依赖已成功安装！"
@@ -28,6 +32,7 @@ else
     echo "依赖安装失败，请检查 Node.js 环境。"
     exit 1
 fi
+
 echo "正在下载 app.js"
 if curl -s -o "$APP_JS_PATH" "$APP_JS_URL"; then
     echo "app.js 文件下载成功"
@@ -35,12 +40,16 @@ else
     echo "下载 app.js 文件失败，请检查下载地址。"
     exit 1
 fi
+
 chmod 644 "$APP_JS_PATH"
 if [[ $? -eq 0 ]]; then
+    echo "文件权限设置成功"
 else
+    echo "文件权限设置失败"
     exit 1
 fi
-echo " 一 键 部 署 己 完 成 。"
+
+echo "一键部署已完成。"
 echo " "
-echo " 保活地址 https://"$DOMAIN"/info "
+echo "保活地址 https://$DOMAIN/info"
 
